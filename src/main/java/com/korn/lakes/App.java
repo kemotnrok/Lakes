d11e8393e07e61362d1861a4b066403110944406
Thomas Korn <thomas.korn@gmx.at>
2024-05-06 22:14:24 +0200

    mac abend

--- src/main/java/com/korn/lakes/App.java
+++ src/main/java/com/korn/lakes/App.java
@@ -13,19 +13,19 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("fxml-views/login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 440, 340);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[32];
-        random.nextBytes(salt);
+//        SecureRandom random = new SecureRandom();
+//        byte[] salt = new byte[32];
+//        random.nextBytes(salt);
+//        String saltString = Base64.getUrlEncoder().encodeToString(salt);
+//        System.out.println(saltString);
 
-        String saltString = Base64.getUrlEncoder().encodeToString(salt);
-        System.out.println(saltString);
         launch();
     }
 }
--- src/main/java/com/korn/lakes/view/LoginController.java
+++ src/main/java/com/korn/lakes/view/LoginController.java
@@ -1,14 +1,24 @@
 package com.korn.lakes.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LoginController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField email;
+
+    @FXML
+    private Hyperlink createAccount;
+
+    @FXML
+    private Button weiter;
+
+    @FXML
+    protected void onCreateAccount() {
+        System.out.println("zum Registrieren");
     }
 }
