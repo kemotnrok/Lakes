e16f329261658b71b7062ddb14daadaecce05bef
Thomas Korn <thomas.korn@gmx.at>
2024-05-08 21:53:14 +0200

    mac

--- src/main/java/com/korn/lakes/App.java
+++ src/main/java/com/korn/lakes/App.java
@@ -3,13 +3,25 @@ package com.korn.lakes;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+//        Font laden
+        try {
+            Font.loadFont(Objects.requireNonNull(getClass().getResource("/com/korn/lakes/font/Rubik-Regular.ttf")).toExternalForm(), 10);
+            Font.loadFont(Objects.requireNonNull(getClass().getResource("/com/korn/lakes/font/Rubik-Bold.ttf")).toExternalForm(), 10);
+        } catch (Exception e) {
+            System.out.println("Keine Schrift geladen, " + e.getMessage());
+        }
+
+//        Szene starten
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view-fxml/view-loginEmail.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 440, 340);
         stage.setTitle("Login");
